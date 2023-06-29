68c160b5ecee71d455e5cc75f145d25e8e869113
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-06-29 10:24:54 +0300

    some fix

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -3,8 +3,8 @@ import { ApiError } from '../api/error';
 
 export const errorCatch = (res: Response, error: unknown, status: number) => {
 	if (error instanceof ApiError) {
-		const apiError = new ApiError(error.message);
-		res.status(status).send(apiError.message);
+		const { message } = new ApiError(error.message);
+		res.status(status).send(message);
 	} else {
 		console.error('An unexpected error occurred:', error);
 	}
